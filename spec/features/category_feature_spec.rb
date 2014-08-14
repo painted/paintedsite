a362cbe66c7b35645fd282a077bbbc3ec920de55
Painted <will.allen@paintedltd.co.uk>
2014-08-14 18:19:33 +0200

    added a category model with update to index page. Must run migrate

--- spec/features/category_feature_spec.rb
+++ spec/features/category_feature_spec.rb
@@ -5,4 +5,14 @@ describe 'categories listing page' do
 		visit '/categories'
 		expect(page).to have_content 'No categories yet'
 	end
+
+	context 'with categories' do 
+		before do
+			Category.create(title: 'Bio')
+		end
+		it 'should show the category' do 
+			visit '/categories'
+			expect(page).to have_content 'Bio'
+		end
+	end
 end
