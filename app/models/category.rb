791497bac13a457459e3d6b71718ed01fdc3c660
Painted <will.allen@paintedltd.co.uk>
2014-09-12 14:02:55 +0100

    Edited display of description of categories to only include the first 15 letters

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ActiveRecord::Base
 	has_attached_file :image,
-	styles: { thumb: '300X300>' },
+	styles: { thumb: '300X300>', large: '600X600>', small: '150X150>', vsmall: '100X100>' },
 	storage: :s3, 
 	s3_credentials: {
 			bucket: 'PaintedLtd',
