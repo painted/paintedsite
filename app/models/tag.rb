d4b4841b0ec0b86368c9075098c366da5075204e
Painted <will.allen@paintedltd.co.uk>
2014-10-07 11:39:35 +0100

    tag names are being used for the url no longer the id

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,8 @@
 class Tag < ActiveRecord::Base
 	has_and_belongs_to_many :categories
 	validates :name, uniqueness: true
+
+	def to_param
+		name
+	end
 end
--- spec/features/tagging_feature_spec.rb
+++ spec/features/tagging_feature_spec.rb
@@ -21,28 +21,37 @@ describe 'tagging category with link' do
 			expect(page).to have_link 'swag'
 		end
 
-		it 'should filter categories by selected tag' do 
+		context 'existing posts' do 
+			before do 
+				visit '/categories/new'
+				fill_in 'Title', with: 'Pic1'
+				fill_in 'Description', with: 'Lorem Ipsum'
+				attach_file 'Image', Rails.root.join('spec/images/paintedicon.jpeg')
+				fill_in 'Url', with: 'http://www.paintedltd.co.uk'
+				fill_in 'Tags', with: 'yolo'
+				click_button 'Submit'
 
-			visit '/categories/new'
-			fill_in 'Title', with: 'Pic1'
-			fill_in 'Description', with: 'Lorem Ipsum'
-			attach_file 'Image', Rails.root.join('spec/images/paintedicon.jpeg')
-			fill_in 'Url', with: 'http://www.paintedltd.co.uk'
-			fill_in 'Tags', with: 'yolo'
-			click_button 'Submit'
+				visit '/categories/new'
+				fill_in 'Title', with: 'Pic2'
+				fill_in 'Description', with: 'Lorem Ipsum'
+				attach_file 'Image', Rails.root.join('spec/images/paintedicon.jpeg')
+				fill_in 'Url', with: 'http://www.paintedltd.co.uk'
+				fill_in 'Tags', with: 'swag'
+				click_button 'Submit'
+			end
 
-			visit '/categories/new'
-			fill_in 'Title', with: 'Pic2'
-			fill_in 'Description', with: 'Lorem Ipsum'
-			attach_file 'Image', Rails.root.join('spec/images/paintedicon.jpeg')
-			fill_in 'Url', with: 'http://www.paintedltd.co.uk'
-			fill_in 'Tags', with: 'swag'
-			click_button 'Submit'
-			
-			click_link 'yolo'
-			expect(page).to have_css 'h1', 'Categories associated with Yolo'
-			expect(page).to have_content 'Pic1'
-			expect(page).not_to have_content 'Pic2'
+
+			it 'should filter categories by selected tag' do 				
+				click_link 'yolo'
+				expect(page).to have_css 'h1', 'Categories associated with Yolo'
+				expect(page).to have_content 'Pic1'
+				expect(page).not_to have_content 'Pic2'
+			end
+
+			it 'uses the tag name in the url' do 
+				click_link 'yolo'
+				expect(current_path).to eq '/tags/yolo'
+			end
 		end
 	end 
 end
